4e0bb6db6e6a075099c2e4d60aad47c1301b727b
Irenej Marc <irenej.marc@dlabs.si>
2017-04-25 13:11:48 +0200

    Remove unneeded append functions

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -3,21 +3,3 @@ function appendMessage(user, body) {
     .append([$('<b>').text(`${user}: `), body])
     .appendTo($("#chat-body"));
 }
-
-function appendSuccess(message) {
-  $('<div class="text-success">')
-    .text(message)
-    .appendTo($("#chat-body"));
-}
-
-function appendInfo(message) {
-  $('<div class="text-info">')
-    .text(message)
-    .appendTo($("#chat-body"));
-}
-
-function appendError(message) {
-  $('<div class="text-danger">')
-    .text(message)
-    .appendTo($("#chat-body"));
-}
